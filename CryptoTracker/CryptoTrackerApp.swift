a5fa7025443bd02a9211c5f9fba9b1afbf60e5ab
Michael Danylchuk <mn_danylchuk@yahoo.com>
2025-03-02 23:54:05 -0800

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Michael Danylchuk on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
